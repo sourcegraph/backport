45df4cabc08ffabcb08cbb5a79a2287aa2f7d6ea
BolajiOlajide <cooproton@gmail.com>
2023-11-28 18:00:59 +0100

    switch to regex-based replace

--- src/utils.ts
+++ src/utils.ts
@@ -1,16 +1,3 @@
 export function stripTestPlanFromPRBody(body: string): string {
-    const startDelimiter = '<!--';
-    const endDelimiter = '-->';
-  
-    let startIndex = body.indexOf(startDelimiter);
-    if (startIndex === -1) {
-      return body; 
-    }
-  
-    let endIndex = body.indexOf(endDelimiter, startIndex + startDelimiter.length);
-    if (endIndex === -1) {
-      return body;
-    }
-  
-    return body.slice(0, startIndex) + body.slice(endIndex + endDelimiter.length); 
+    return body.replace(/<!--[\s\S]*?-->/g, ''); 
 };
