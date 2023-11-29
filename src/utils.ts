9f43e17af0cf3d33a32f158a4cce01df27466c9a
BolajiOlajide <cooproton@gmail.com>
2023-11-29 15:28:59 +0100

    change to arrow func

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,3 @@
-export function stripTestPlanFromPRBody(body: string): string {
+export const stripTestPlanFromPRBody = (body: string): string => {
   return body.replace(/<!--[\s\S]*?-->/g, "");
 }
